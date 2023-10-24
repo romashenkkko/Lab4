922d755272322ebbc4b14010fb943e8068a131a7
romashenkkko <70690141+romashenkkko@users.noreply.github.com>
2023-10-24 14:33:27 +0300

    Add files via upload

---
+++ kulev_task.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char user_sentence[1000];
+    printf("Enter a sentence: ");
+    fgets(user_sentence, sizeof(user_sentence), stdin);
+
+    // Make a copy of the input sentence to avoid modifying the original
+    char sentence_copy[1000];
+    strcpy(sentence_copy, user_sentence);
+
+    char *separator = strtok(sentence_copy, " ,\n"); // Include '\n' to handle newline characters
+
+    char unique_words[1000][50];
+    int word_counter[1000] = {0};
+    int unique_word_count = 0;
+
+    while (separator != NULL) {
+        int is_unique = 1;
+
+        // Remove any unwanted characters like '\n' from the token
+        int len = strlen(separator);
+        if (separator[len - 1] == '\n') {
+            separator[len - 1] = '\0';
+        }
+
+        for (int i = 0; i < unique_word_count; i++) {
+            if (strcmp(separator, unique_words[i]) == 0) {
+                word_counter[i]++;
+                is_unique = 0;
+                break;
+            }
+        }
+
+        if (is_unique) {
+            strcpy(unique_words[unique_word_count], separator);
+            word_counter[unique_word_count]++;
+            unique_word_count++;
+        }
+
+        separator = strtok(NULL, " ,\n");
+    }
+
+    printf("Unique words in the sentence: ");
+    for (int i = 0; i < unique_word_count; i++) {
+        printf("%s ", unique_words[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
